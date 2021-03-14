d45782ad7119b7a6849e200e6754d54a4d2e5e2e
TiltedWaechter <marcel.albsmeier@gmail.com>
2021-03-14 17:59:57 +0100

    enabled old input system, check mouseclick on tiles

--- Assets/Scripts/Misc/Waypoint.cs
+++ Assets/Scripts/Misc/Waypoint.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Waypoint : MonoBehaviour{
-    // Start is called before the first frame update
-    void Start(){
-        
-    }
 
-    // Update is called once per frame
-    void Update(){
-        
+    [SerializeField] bool towerCanBePlacedHere;
+
+
+    void OnMouseDown() {
+
+        if (towerCanBePlacedHere) {
+            Debug.Log(transform.name);
+        }
+           
     }
 }
