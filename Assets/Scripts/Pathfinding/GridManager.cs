b0813a13a6b962cc78cbbacdab283272178a9682
TiltedWaechter <marcel.albsmeier@gmail.com>
2021-03-19 00:20:38 +0100

    added way to access nodes via gridmanager

--- Assets/Scripts/Pathfinding/GridManager.cs
+++ Assets/Scripts/Pathfinding/GridManager.cs
@@ -16,13 +16,20 @@ public class GridManager : MonoBehaviour{
         CreateGrid();
     }
 
+    public Node GetNode(Vector2Int coordinates) {
+        if (grid.ContainsKey(coordinates)) {
+            return grid[coordinates];
+        }
+        return null;
+    }
+
     private void CreateGrid() {
 
         for (int x = 0; x < gridSize.x; x++) {
             for (int y = 0; y < gridSize.y; y++ ) {
                 Vector2Int coordinates = new Vector2Int(x,y);
                 grid.Add(coordinates, new Node(coordinates, true));
-                Debug.Log(grid[coordinates].coordinates + " = " + grid[coordinates].canBeWalkedOn);
+               
             }
 
         }
