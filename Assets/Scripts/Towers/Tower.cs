6a72cedd6ce022cc270a659934fe3ad80f55b8bf
TiltedWaechter <marcel.albsmeier@gmail.com>
2021-03-21 02:52:40 +0100

    build timer

--- Assets/Scripts/Towers/Tower.cs
+++ Assets/Scripts/Towers/Tower.cs
@@ -7,6 +7,7 @@ public class Tower : MonoBehaviour{
 
     //parameters
     [SerializeField] int cost = 50;
+    [SerializeField] float buildTimer = 2f;
 
     //constants
     const string TOWERS_PARENT_NAME = "Towers";
@@ -20,6 +21,10 @@ public class Tower : MonoBehaviour{
 
     private void Awake() {
         GenerateTowerParent();
+    }
+
+    private void Start() {
+        StartCoroutine(Build());   
     }
 
     private void GenerateTowerParent() {
@@ -40,5 +45,37 @@ public class Tower : MonoBehaviour{
         thisTower.transform.SetParent(towersParent.transform);
         return true;
 
+    }
+
+    IEnumerator Build() {
+        float delayPerPart;
+        List<Transform> components = new List<Transform>();
+
+        foreach (Transform part in transform) {
+            Debug.Log(part.gameObject.name);
+            foreach (Transform child in part ) {
+                child.gameObject.SetActive(false);
+                Debug.Log(child.gameObject.name);
+                components.Add(child);
+                foreach (Transform grandchild in child) {
+                    Debug.Log(grandchild.gameObject.name);
+                    grandchild.gameObject.SetActive(false);
+                    components.Add(grandchild);
+                }
+            }
+        }
+
+        delayPerPart = buildTimer / components.Count;
+
+        foreach (Transform part in transform) {
+            foreach (Transform child in part) {
+                child.gameObject.SetActive(true);
+                yield return new WaitForSeconds(delayPerPart);
+                foreach (Transform grandchild in child) {
+                    grandchild.gameObject.SetActive(true);
+                    yield return new WaitForSeconds(delayPerPart);
+                }
+            }
+        }
     }
 }
